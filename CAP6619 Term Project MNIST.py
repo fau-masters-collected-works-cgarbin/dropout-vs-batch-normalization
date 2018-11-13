465b93dbe97932bba41bfefe8161037214cd38af
cgarbin <garbin.christian@gmail.com>
2018-11-13 09:23:21 -0500

    First version of all network configurations

--- CAP6619 Term Project MNIST.py
+++ CAP6619 Term Project MNIST.py
@@ -13,15 +13,30 @@ from keras.datasets import mnist
 # Store data from the experiments
 experiments = pd.DataFrame(columns=["Description", "DataSetName", "Loss",
                                     "Accuracy", "Epochs", "BatchSize",
-                                    "TrainingTime", "TestTime", "ModelJson"])
+                                    "TrainingCpuTime", "TestCpuTime"])
 
 
 def add_experiment(description, data_set_name, loss, accuracy, epochs,
-                   batch_size, training_time, test_time, model_json):
+                   batch_size, training_time, test_time):
     """Add an entry to the dataframe that keeps track of experiments."""
     new_row = [description, data_set_name, loss, accuracy, epochs,
-               batch_size, training_time, test_time, model_json]
+               batch_size, training_time, test_time]
     experiments.loc[len(experiments)] = new_row
+
+
+def run_experiment(model):
+    """Run an experiment: train and est the network"""
+    start = time.process_time()
+    epochs = 2
+    batch_size = 128
+    model.fit(train_images, train_labels, epochs=epochs, batch_size=batch_size)
+    training_time = time.process_time() - start
+
+    start = time.process_time()
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    test_time = time.process_time() - start
+
+    return test_loss, test_acc, epochs, batch_size, training_time, test_time
 
 
 # Load and prepare data
@@ -32,6 +47,12 @@ test_labels = to_categorical(test_labels)
 end = time.process_time()
 print("Timing: load and prepare data: {0:.5f}s".format(end - start))
 
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype('float32') / 255
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype('float32') / 255
+
+# Standard network -----------------------------------------------------------
 model = models.Sequential()
 model.add(layers.Dense(1024, activation='relu', input_shape=(28 * 28,)))
 model.add(layers.Dense(1024, activation='relu'))
@@ -41,24 +62,102 @@ model.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
-train_images = train_images.reshape((60000, 28 * 28))
-train_images = train_images.astype('float32') / 255
-test_images = test_images.reshape((10000, 28 * 28))
-test_images = test_images.astype('float32') / 255
+test_loss, test_acc, epochs, batch_size, training_time, test_time = \
+    run_experiment(model)
 
-start = time.process_time()
-epochs = 2
-batch_size = 128
-model.fit(train_images, train_labels, epochs=epochs, batch_size=batch_size)
-training_time = time.process_time()
-print("Timing: fit model: {0:.5f}s".format(end - start))
+add_experiment("Standard network, 1024 nodes", "MNIST", test_loss, test_acc,
+               epochs, batch_size, training_time, test_time)
 
-start = time.process_time()
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-test_time = time.process_time()
-print("Timing: evaluate model: {0:.5f}s".format(end - start))
-print('test_acc:', test_acc)
+# Dropout network, no adjustemt ----------------------------------------------
+dropout_rate = 0.5
+model = models.Sequential()
+model.add(layers.Dense(1024, activation='relu', input_shape=(28 * 28,)))
+model.add(layers.Dropout(rate=dropout_rate))
+model.add(layers.Dense(1024, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
 
-add_experiment("Standard network", "MNIST", test_loss, test_acc, epochs,
-               batch_size, training_time, test_time, model.to_json())
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+test_loss, test_acc, epochs, batch_size, training_time, test_time = \
+    run_experiment(model)
+
+add_experiment("Dropout network not adjusted, 1024 nodes", "MNIST", test_loss, test_acc,
+               epochs, batch_size, training_time, test_time)
+
+# Dropout network adjusted before -----------------------------------------------------
+model = models.Sequential()
+model.add(layers.Dense(int(1024 / dropout_rate),
+                       activation='relu', input_shape=(28 * 28,)))
+model.add(layers.Dropout(rate=dropout_rate))
+model.add(layers.Dense(1024, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+test_loss, test_acc, epochs, batch_size, training_time, test_time = \
+    run_experiment(model)
+
+add_experiment("Dropout network adjusted before, 1024 nodes", "MNIST",
+               test_loss, test_acc, epochs, batch_size, training_time,
+               test_time)
+
+# Dropout network, adjusted after --------------------------------------------
+model = models.Sequential()
+model.add(layers.Dense(1024, activation='relu', input_shape=(28 * 28,)))
+model.add(layers.Dropout(rate=dropout_rate))
+model.add(layers.Dense(int(1024 / dropout_rate), activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+test_loss, test_acc, epochs, batch_size, training_time, test_time = \
+    run_experiment(model)
+
+add_experiment("Dropout network adjusted after, 1024 nodes", "MNIST",
+               test_loss, test_acc, epochs, batch_size, training_time,
+               test_time)
+
+# Dropout network, adjusted all layers ---------------------------------------
+model = models.Sequential()
+model.add(layers.Dense(int(1024 / dropout_rate),
+                       activation='relu', input_shape=(28 * 28,)))
+model.add(layers.Dropout(rate=dropout_rate))
+model.add(layers.Dense(int(1024 / dropout_rate), activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+test_loss, test_acc, epochs, batch_size, training_time, test_time = \
+    run_experiment(model)
+
+add_experiment("Dropout network adjusted all, 1024 nodes", "MNIST", test_loss,
+               test_acc, epochs, batch_size, training_time, test_time)
+
+# Dropout network, dropout before output layer -------------------------------
+model = models.Sequential()
+model.add(layers.Dense(int(1024 / dropout_rate),
+                       activation='relu', input_shape=(28 * 28,)))
+model.add(layers.Dropout(rate=dropout_rate))
+model.add(layers.Dense(int(1024 / dropout_rate), activation='relu'))
+model.add(layers.Dropout(rate=dropout_rate))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+test_loss, test_acc, epochs, batch_size, training_time, test_time = \
+    run_experiment(model)
+
+add_experiment("Dropout network all layers, 1024 nodes", "MNIST", test_loss,
+               test_acc, epochs, batch_size, training_time, test_time)
+
 print(experiments)
