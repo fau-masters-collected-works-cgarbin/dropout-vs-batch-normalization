c7f6d1ee28a74be6c178fda5a43da8fba9b257b2
cgarbin <garbin.christian@gmail.com>
2018-11-28 07:56:08 -0500

    Show top 10 results by accuracy

--- mnist/dropout/CAP6619_term_project_mnist_dnn_dropout_analysis.py
+++ mnist/dropout/CAP6619_term_project_mnist_dnn_dropout_analysis.py
@@ -14,6 +14,15 @@ file_generator = (pd.read_csv(f, delim_whitespace=True) for f in all_files)
 # Read all files using the generator and concatenates them (ignore_index=True
 # creates a unique index across all files)
 results = pd.concat(file_generator, ignore_index=True)
-# Uncomment to save combined results into one file
-# with open("results.txt", "w") as f:
-#     results.to_string(f)
+
+# Save combined results into files
+with open("results_all.txt", "w") as f:
+    results.to_string(f)
+with open("results_top10_accuracy_overall.txt", "w") as f:
+    results.nlargest(10, "TestAccuracy").to_string(f)
+with open("results_top10_accuracy_dropout.txt", "w") as f:
+    results.loc[results["Description"] != "standard_network"]. \
+        nlargest(10, "TestAccuracy").to_string(f)
+with open("results_top10_accuracy_no_dropout.txt", "w") as f:
+    results.loc[results["Description"] == "standard_network"]. \
+        nlargest(10, "TestAccuracy").to_string(f)
