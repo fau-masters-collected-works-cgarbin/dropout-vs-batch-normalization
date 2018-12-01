38a88d6302bacc4e7c2d5fd0ffdc14c2057ea784
cgarbin <garbin.christian@gmail.com>
2018-12-01 18:03:50 -0500

    Switch to training one model at a time
    Create only one model, as described by the parameters,
    train and test it.
    
    This avoids duplicaiton of model training.

--- mnist/dropout/CAP6619_term_project_mnist_dnn_dropout.py
+++ mnist/dropout/CAP6619_term_project_mnist_dnn_dropout.py
@@ -16,7 +16,7 @@ from keras.constraints import max_norm
 from keras.datasets import mnist
 
 
-def test_model(description, model, parameters, end_experiment_callback):
+def test_model(model, parameters, end_experiment_callback):
     """Test one model: train it, evaluate with test data, save results."""
     # To make lines shorter
     p = parameters
@@ -30,69 +30,72 @@ def test_model(description, model, parameters, end_experiment_callback):
     test_loss, test_acc = model.evaluate(test_images, test_labels)
     test_time = time.process_time() - start
 
-    end_experiment_callback(description, parameters, model, test_loss,
+    end_experiment_callback(parameters, model, test_loss,
                             test_acc, training_time, test_time)
 
 
-def test_network_configurations(parameters,
-                                standard_optimizer, dropout_optimizer,
-                                end_experiment_callback):
-    """Test all network configurations with the given parameters."""
+def test_network(parameters, end_experiment_callback):
+    """Test a network described by the given parameters."""
     # To make lines shorter
     p = parameters
 
-    # Standard network (no dropout)
     model = models.Sequential()
-    model.add(layers.Dense(p.units_per_layer,
-                           activation='relu', input_shape=(pixels_per_image,)))
-    for _ in range(p.hidden_layers - 1):
-        model.add(layers.Dense(p.units_per_layer, activation='relu'))
-    model.add(layers.Dense(10, activation='softmax'))
-    model.compile(optimizer=standard_optimizer,
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-    test_model("standard_network", model, p, end_experiment_callback)
 
-    # Dropout without adjustment to number of units (for comparison - not in
-    # the original paper).
-    # Dropout is applied to all layers, as shown in figure 1.b in the paper.
-    model = models.Sequential()
-    model.add(layers.Dropout(p.dropout_rate_input_layer,
-                             input_shape=(pixels_per_image,)))
-    for _ in range(p.hidden_layers):
-        model.add(layers.Dense(p.units_per_layer, activation='relu',
-                               kernel_initializer='he_normal',
-                               kernel_constraint=max_norm(int(p.max_norm_max_value))))
-    model.add(layers.Dropout(rate=p.dropout_rate_hidden_layer))
-    model.add(layers.Dense(10, activation='softmax'))
-    model.compile(optimizer=dropout_optimizer,
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-    test_model("dropout_no_adjustment", model, p, end_experiment_callback)
+    if p.network == "standard":
+        model.add(layers.Dense(p.units_per_layer,
+                               activation='relu',
+                               input_shape=(pixels_per_image,)))
+        for _ in range(p.hidden_layers - 1):
+            model.add(layers.Dense(p.units_per_layer, activation='relu'))
+    elif p.network in ("dropout_no_adjustment", "dropout"):
+        units_hidden_layer = 0
+        if p.network == "dropout":
+            # Adjust number of units in each layer: "...if an n-sized layer is
+            # optimal for a standard neural net on any given task, a good
+            # dropout net should have at least n/p units." [Note that Keras
+            # uses a "drop" rate, not "keep", hence the "1 -"].
+            units_hidden_layer = int(
+                p.units_per_layer / (1 - p.dropout_rate_hidden_layer))
+        else:
+            units_hidden_layer = p.units_per_layer
 
-    # Adjust number of units in each layer: "...if an n-sized layer is optimal
-    # for a standard neural net on any given task, a good dropout net should
-    # have at least n/p units." [Keras is "drop", not "keep", hence the "1 -"].
-    adjusted_units_hidden = int(
-        p.units_per_layer / (1 - p.dropout_rate_hidden_layer))
-
-    # Dropout with adjustment to number of units.
-    # Dropout is applied to all layers, as shown in figure 1.b in the paper.
-    # See also http://www.cs.toronto.edu/~nitish/dropout/mnist.pbtxt for code
-    # used in the paper for more details on some of the parameters.
-    model = models.Sequential()
-    model.add(layers.Dropout(p.dropout_rate_input_layer,
-                             input_shape=(pixels_per_image,)))
-    for _ in range(p.hidden_layers):
-        model.add(layers.Dense(adjusted_units_hidden, activation='relu',
-                               kernel_initializer='he_normal',
-                               kernel_constraint=max_norm(int(p.max_norm_max_value))))
+        model.add(layers.Dropout(p.dropout_rate_input_layer,
+                                 input_shape=(pixels_per_image,)))
+        for _ in range(p.hidden_layers):
+            model.add(layers.Dense(units_hidden_layer, activation='relu',
+                                   kernel_initializer='he_normal',
+                                   kernel_constraint=max_norm(
+                                       int(p.max_norm_max_value))))
         model.add(layers.Dropout(rate=p.dropout_rate_hidden_layer))
+    else:
+        assert False  # Invalid network type
+
+    # All networks end with the sofmax layer to identify the 0-9 digits
     model.add(layers.Dense(10, activation='softmax'))
-    model.compile(optimizer=dropout_optimizer,
+
+    # Create the optimizer
+    optimizer = None
+    if p.optimizer == "sgd":
+        if p.momentum == "none":
+            optimizer = optimizers.SGD(p.learning_rate)
+        else:
+            optimizer = optimizers.SGD(
+                p.learning_rate, momentum=float(p.momentum))
+    elif p.optimizer == "rmsprop":
+        if p.momentum == "none":
+            optimizer = optimizers.RMSprop(p.learning_rate)
+        else:
+            optimizer = optimizers.RMSprop(
+                p.learning_rate, momentum=float(p.momentum))
+    else:
+        assert False  # Invalid optimizer
+
+    # Compile the network and test it
+    model.compile(optimizer=optimizer,
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
-    test_model("dropout_units_adjusted", model, p, end_experiment_callback)
+
+    test_model(model, p, end_experiment_callback)
 
     # A note on L2 regularization: although the paper says it was "found to be
     # useful for dropout neural networks as well", during the tests it didn't
@@ -101,7 +104,7 @@ def test_network_configurations(parameters,
     # model.add(...kernel_regularizer=regularizers.l2(0.001),
 
 
-def save_experiment(description, parameters, model, test_loss, test_acc,
+def save_experiment(parameters, model, test_loss, test_acc,
                     training_time, test_time):
     """Save results from one experiment."""
     # To save some typing
@@ -130,7 +133,7 @@ def save_experiment(description, parameters, model, test_loss, test_acc,
     # File where the results will be saved (the name encodes the parameters
     # used in the experiments)
     base_name_prefix = "MNIST_DNN_Dropout"
-    base_name_template = ("{}nw={}_opt={}_hl={:03d}_uhl={:04d}_e={:02d}"
+    base_name_template = ("{}_nw={}_opt={}_hl={:03d}_uhl={:04d}_e={:02d}"
                           "_bs={:04d}_dri={:0.2f}_drh={:0.2f}_lr={:03.1f}"
                           "_m={}_mn={}")
     base_name = base_name_template.format(
@@ -148,7 +151,7 @@ def save_experiment(description, parameters, model, test_loss, test_acc,
     # object with the training results for each epoch.
     # We need to save the history separately because `model.save` won't save
     # it - it saves only the model data.
-    results_file = base_name + "_" + description + "_" + optimizer_name + "_"
+    results_file = base_name + "_" + p.network + "_" + optimizer_name + "_"
     import json
     with open(results_file + "history.json", 'w') as f:
         json.dump(model.history.history, f)
@@ -191,41 +194,6 @@ def parse_command_line():
     )
 
 
-def run_all_experiments(parameters):
-    """Run all experiments: test all network configurations, with different
-    optimizers."""
-    # The SGD optimizer to use in standard networks (no dropout).
-    optimizer_sgd_standard = optimizers.SGD()
-    # The SGD optimizer to use in dropout networks.
-    optimizer_sgd_dropout = optimizers.SGD(
-        lr=backend.eval(optimizer_sgd_standard.lr) * p.learning_rate,
-        momentum=float(p.momentum))
-
-    # The RMSProp optimizer to use in standard networks (no dropout).
-    # The paper doesn't mention what optimizer was used in the tests. It looks
-    # like those tests were done with SGD. I tried RMSProp here because it's a
-    # popular one nowadays and the one used in the Deep Learning With Python
-    # book. It results in good accuracy with the default learning rate, even
-    # before dropout is applied.
-    optimizer_rmsprop_standard = optimizers.RMSprop()
-    # The RMSProp optimizer to use in dropout networks.
-    # Increasing the learn rate for the RMSProp optimizer resulted in much
-    # worse accuracy. To prevent that we use the default optimizer for dropout.
-    optimizer_rmsprop_dropout = optimizer_rmsprop_standard
-
-    # Run the experiments with the SGD optimzer
-    test_network_configurations(parameters,
-                                standard_optimizer=optimizer_sgd_standard,
-                                dropout_optimizer=optimizer_sgd_dropout,
-                                end_experiment_callback=save_experiment)
-
-    # Run the experiments with the RMSProp optimizer
-    test_network_configurations(parameters,
-                                standard_optimizer=optimizer_rmsprop_standard,
-                                dropout_optimizer=optimizer_rmsprop_dropout,
-                                end_experiment_callback=save_experiment)
-
-
 # Store data from the experiments
 experiments = pd.DataFrame(columns=["DataSetName", "Network", "Optimizer",
                                     "TestLoss", "TestAccuracy",
@@ -244,6 +212,9 @@ Parameters = collections.namedtuple("Parameters", [
     "network",
     # Type of optimizer to use: "sgd" or "rmsprop". The paper doesn't specify,
     # but it can be inferred that it's an SGD with adjusted learning rate.
+    # I also tried RMSProp here because it's a popular one nowadays and the one
+    # used in the Deep Learning With Python book. It results in good accuracy
+    # with the default learning rate, even before dropout is applied.
     "optimizer",
     # Number of hidden layers in the network. When a dropout network is used,
     # each hidden layer will be followed by a dropout layer.
@@ -268,7 +239,8 @@ Parameters = collections.namedtuple("Parameters", [
     "learning_rate",
     # Momentum, to adjust as recommended in the dropout paper ("While momentum
     # values of 0.9 are common for standard nets, with dropout we found that
-    # values around 0.95 to 0.99 work quite a lot better.")
+    # values around 0.95 to 0.99 work quite a lot better."), or "none" to skip
+    # customization and use the default from Keras
     "momentum",
     # Max norm max value, or "none" to skip it. The paper recommends its usage
     # ("Although dropout alone gives significant improvements, using dropout
@@ -295,4 +267,4 @@ test_images = test_images.reshape((10000, pixels_per_image))
 test_images = test_images.astype('float32') / 255
 
 p = parse_command_line()
-run_all_experiments(p)
+test_network(p, save_experiment)
