8c8f9cf23a8436a9d48eb0fa536f0793add56523
cgarbin <garbin.christian@gmail.com>
2018-12-02 18:39:02 -0500

    Move code to create script file into a function
    We will need this later, when supporting more than one
    experiment in the generator.

--- mnist/dropout/CAP6619_term_project_mnist_dnn_dropout_test_generator.py
+++ mnist/dropout/CAP6619_term_project_mnist_dnn_dropout_test_generator.py
@@ -5,12 +5,10 @@ MNIST with standard deep neural network and dropout
 Create a shell script with all tests we need to execute.
 """
 import itertools
-import os
-import stat
 
 # Some default values from Keras to keep in mind:
 #  * Learning rate: SGD=0.01, RMSprop=0.001
-#  * Momenum: SGD=0.0 (RMSprop doesn't have momentum)
+#  * Momentum: SGD=0.0 (RMSprop doesn't have momentum)
 #  * Decay: 0.0 for SGD and RMSprop
 #  * MaxNorm:  not used in either, must be explicitly added
 
@@ -44,28 +42,36 @@ sgd_momentum = ["0.95"]
 max_norm_max_value = ["2"]
 
 
+def create_test_file(file_name, experiment_name, tests):
+    import os
+    import stat
+
+    args_template = (
+        "--experiment_name {} --network {} --optimizer {} --hidden_layers {} "
+        "--units_per_layer {} --epochs {} --batch_size {} "
+        "--dropout_rate_input_layer {} --dropout_rate_hidden_layer {} "
+        "--learning_rate {} --decay {} --sgd_momentum {} "
+        "--max_norm_max_value {}")
+    with open(file_name, "w") as f:
+        f.write("#!/bin/bash\n")
+        f.write("# This file was automatically generated\n\n")
+        for i, test in enumerate(tests, start=1):
+            args = args_template.format(
+                experiment_name,
+                test[0], test[1], test[2], test[3], test[4], test[5], test[6],
+                test[7], test[8], test[9], test[10], test[11])
+            f.write('echo "{} - test {} of {} - {}"\n'.format(
+                experiment_name, i, len(all_tests), test))
+            f.write("python3 CAP6619_term_project_mnist_dnn_dropout.py \\\n")
+            f.write("   " + args + "\n\n")
+
+    # Make it executable (for the current user)
+    os.chmod(file_name, os.stat(file_name).st_mode | stat.S_IEXEC)
+
+
 all_tests = list(itertools.product(
     network, optimizer, hidden_layers, units_per_layer, epochs, batch_size,
     dropout_rate_input_layer, dropout_rate_hidden_layer, learning_rate,
     decay, sgd_momentum, max_norm_max_value))
 
-args_template = (
-    "--network {} --optimizer {} --hidden_layers {} --units_per_layer {} "
-    "--epochs {} --batch_size {} --dropout_rate_input_layer {} "
-    "--dropout_rate_hidden_layer {} --learning_rate {} --decay {}"
-    " --sgd_momentum {} --max_norm_max_value {}")
-script_file = "mnist_tests.sh"
-with open(script_file, "w") as f:
-    f.write("#!/bin/bash\n")
-    f.write("# This file was automatically generated\n\n")
-    for i, test in enumerate(all_tests, start=1):
-        args = args_template.format(
-            test[0], test[1], test[2], test[3], test[4], test[5], test[6],
-            test[7], test[8], test[9], test[10], test[11])
-        f.write('echo "Testing {} of {} - {}"\n'.format(i, len(all_tests),
-                                                        test))
-        f.write("python3 CAP6619_term_project_mnist_dnn_dropout.py \\\n")
-        f.write("   " + args + "\n\n")
-
-# Make it executable (for the current user)
-os.chmod(script_file, os.stat(script_file).st_mode | stat.S_IEXEC)
+create_test_file("mnist_tests.sh", "dropout_mnist_dnn", all_tests)
