35967487c9ef9bffaa8b26d89b3cc5969266c94b
cgarbin <garbin.christian@gmail.com>
2018-12-03 13:28:47 -0500

    Change test generator to use Parameters
    In preparation to genreate more test scenarios.

--- mnist/dropout/CAP6619_term_project_mnist_dnn_dropout_test_generator.py
+++ mnist/dropout/CAP6619_term_project_mnist_dnn_dropout_test_generator.py
@@ -2,11 +2,12 @@
 CAP-6619 Deep Learning Fall 2018 term project
 MNIST with standard deep neural network and dropout
 
-Create a shell script with all tests we need to execute.
+Create shell scripts with all tests we need to execute.
 """
 import itertools
 import os
 import stat
+from CAP6619_term_project_mnist_dropout_parameters import Parameters
 
 # Some default values from Keras to keep in mind:
 #  * Learning rate: SGD=0.01, RMSprop=0.001
@@ -14,53 +15,50 @@ import stat
 #  * Decay: 0.0 for SGD and RMSprop
 #  * MaxNorm:  not used in either, must be explicitly added
 
-# All combinations of values we need to try
-# This is the complete list - uncomment for final tests
-# network = ["standard", "dropout_no_adjustment", "dropout"]
-# optimizer = ["sgd", "rmsprop"]
-# hidden_layers = ["1", "2", "3", "4"]
-# units_per_layer = ["512", "1024", "2048"]
-# epochs = ["2", "5", "10"]
-# batch_size = ["128", "256"]
-# dropout_rate_input_layer = ["0.1", "0.2"]
-# dropout_rate_hidden_layer = ["0.5"]
-# learning_rate = ["0.001", "0.01", "0.1"]
-# decay = ["0.001", "0.01"]
-# sgd_momentum = ["0.95", "0.99"]
-# max_norm_max_value = ["none", "2", "3", "4"]
-
-# This is a simplified list
-network = ["standard", "dropout_no_adjustment", "dropout"]
-optimizer = ["sgd", "rmsprop"]
-hidden_layers = ["1"]
-units_per_layer = ["512"]
-epochs = ["2"]
-batch_size = ["128"]
-dropout_rate_input_layer = ["0.1"]
-dropout_rate_hidden_layer = ["0.5"]
-learning_rate = ["0.01"]
-decay = ["0.001"]
-sgd_momentum = ["0.95"]
-max_norm_max_value = ["2"]
+# This is a quick set of tests to test the overall sanity of the code.
+quick_test = Parameters(
+    experiment_name="dropout_mnist_dnn_quick_test",
+    network=["standard", "dropout_no_adjustment", "dropout"],
+    optimizer=["sgd", "rmsprop"],
+    hidden_layers=["1"],
+    units_per_layer=["512"],
+    epochs=["2"],
+    batch_size=["128"],
+    dropout_rate_input_layer=["0.1"],
+    dropout_rate_hidden_layer=["0.5"],
+    learning_rate=["0.01"],
+    decay=["0.001"],
+    sgd_momentum=["0.95"],
+    max_norm_max_value=["2"],
+)
 
 
-def create_test_file(file_name, experiment_name, tests):
+def create_test_file(p):
+
+    tests = list(itertools.product(
+        p.network, p.optimizer, p.hidden_layers, p.units_per_layer, p.epochs,
+        p.batch_size, p.dropout_rate_input_layer, p.dropout_rate_hidden_layer,
+        p.learning_rate, p.decay, p.sgd_momentum, p.max_norm_max_value))
+
     args_template = (
         "--experiment_name {} --network {} --optimizer {} --hidden_layers {} "
         "--units_per_layer {} --epochs {} --batch_size {} "
         "--dropout_rate_input_layer {} --dropout_rate_hidden_layer {} "
         "--learning_rate {} --decay {} --sgd_momentum {} "
         "--max_norm_max_value {}")
+
+    file_name = p.experiment_name + ".sh"
+
     with open(file_name, "w") as f:
         f.write("#!/bin/bash\n")
         f.write("# This file was automatically generated\n\n")
         for i, test in enumerate(tests, start=1):
             args = args_template.format(
-                experiment_name,
+                p.experiment_name,
                 test[0], test[1], test[2], test[3], test[4], test[5], test[6],
                 test[7], test[8], test[9], test[10], test[11])
             f.write('echo "{} - test {} of {} - {}"\n'.format(
-                experiment_name, i, len(all_tests), test))
+                p.experiment_name, i, len(tests), test))
             f.write("python3 CAP6619_term_project_mnist_dnn_dropout.py \\\n")
             f.write("   " + args + "\n\n")
 
@@ -68,9 +66,4 @@ def create_test_file(file_name, experiment_name, tests):
     os.chmod(file_name, os.stat(file_name).st_mode | stat.S_IEXEC)
 
 
-all_tests = list(itertools.product(
-    network, optimizer, hidden_layers, units_per_layer, epochs, batch_size,
-    dropout_rate_input_layer, dropout_rate_hidden_layer, learning_rate,
-    decay, sgd_momentum, max_norm_max_value))
-
-create_test_file("mnist_tests.sh", "dropout_mnist_dnn", all_tests)
+create_test_file(quick_test)
